ac4dff0c5d35344089c536c30a46c6f2e56b908a
AkashPak <144455859+AkashPak@users.noreply.github.com>
2024-02-29 08:49:17 -0500

    sa

--- vite-project/src/components/PushEen.vue
+++ vite-project/src/components/PushEen.vue
@@ -1,15 +1,11 @@
 <template>
-  <div class="item">
-    <i>
-      <slot name="icon"></slot>
-    </i>
-    <div class="details">
-      <h3>
-        <slot name="heading"></slot>
-      </h3>
-      <slot></slot>
-    </div>
-  </div>
+ <div>
+<h1>{{
+
+
+
+
+ </div>
 </template>
 
 <style scoped>

9079b3b2e07823b04cdf679934d6b52e721c74f5
AkashPak <akashp8@nycstudents.net>
2024-03-11 08:58:37 -0400

    dsa

--- vite-project/src/components/PushEen.vue
+++ vite-project/src/components/PushEen.vue
@@ -1,94 +1,39 @@
 <template>
- <div>
-<h1>{{ push.name }} </h1>
-<img :src="push.img" alt=""/>
-<!-- <button @click="addtocart(push)">Add to Kitty Kart</button> -->
- </div>
-
-</template>
-
-<!-- <script setup>
-import { defineEmits } from 'vue';
-
-const props = defineProps({
-  push: Object
-});
-const addtocart= send('addtocart', props.push); /// takes argument of add to cart and applies props.push
-const send= defineEmits(['addtocart']); // defines argument
-
-</script> -->
-
-
-<style scoped>
-
-.item {
-  margin-top: 2rem;
-  display: flex;
-  position: relative;
-}
-
-.details {
-  flex: 1;
-  margin-left: 1rem;
-}
-
-i {
-  display: flex;
-  place-items: center;
-  place-content: center;
-  width: 32px;
-  height: 32px;
-  color: var(--color-text);
-}
-
-h3 {
-  font-size: 1.2rem;
-  font-weight: 500;
-  margin-bottom: 0.4rem;
-  color: var(--color-heading);
-}
-
-@media (min-width: 1024px) {
-  .item {
-    margin-top: 0;
-    padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
+  <div class="flex-container">
+ <h1>{{ kat.name }} </h1>
+ <img class="img" :src="kat.img" alt=""/>
+ <button @click="addItem">Add to Kitty Kart</button>
+  </div>
+ 
+ </template>
+ 
+  <script setup>
+ import { defineEmits ,defineProps } from 'vue'; 
+ 
+ const props = defineProps({
+   kat: Object
+ });
+ const emit = defineEmits(['AddItem']);
+ const addItem=()=>{
+   emit('AddItem' , props.kat);
+ };
+ 
+ </script>
+ 
+ 
+  <style scoped>
+  .img{
+ height:20% ;
+ width:30%
+  };
+  .flex-container{
+   display: flex;
+ flex-wrap: wrap;
+   flex-direction: column;
+   
   }
-
-  i {
-    top: calc(50% - 25px);
-    left: -26px;
-    position: absolute;
-    border: 1px solid var(--color-border);
-    background: var(--color-background);
-    border-radius: 8px;
-    width: 50px;
-    height: 50px;
-  }
-
-  .item:before {
-    content: ' ';
-    border-left: 1px solid var(--color-border);
-    position: absolute;
-    left: 0;
-    bottom: calc(50% + 25px);
-    height: calc(50% - 25px);
-  }
-
-  .item:after {
-    content: ' ';
-    border-left: 1px solid var(--color-border);
-    position: absolute;
-    left: 0;
-    top: calc(50% + 25px);
-    height: calc(50% - 25px);
-  }
-
-  .item:first-of-type:before {
-    display: none;
-  }
-
-  .item:last-of-type:after {
-    display: none;
-  }
-}
-</style>
+ </style>
+ 
+ 
+ 
+ 
